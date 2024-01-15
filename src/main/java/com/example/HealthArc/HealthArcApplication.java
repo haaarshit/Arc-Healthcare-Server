781e0d0fe626bc2729ea6f0acf0c2d4ab8759646
coderharsx1122 <harshit.tripathihero15@gmail.com>
2024-01-15 17:42:18 +0530

    init

---
+++ src/main/java/com/example/HealthArc/HealthArcApplication.java
@@ -0,0 +1,13 @@
+package com.example.HealthArc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthArcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthArcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HealthArc/HealthArcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HealthArc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthArcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
